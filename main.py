9624c33089dab99a6e7ef921b6eb983b5ad8a001
chamoyo <chamoyosoy@hotmail.com>
2025-07-06 05:40:44 -0600

    remove unused code

--- main.py
+++ main.py
@@ -25,18 +25,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        # player.update(dt)
         updatable.update(dt)
 
         screen.fill("black")
 
-        # player.draw(screen)
         for i in drawable:
             i.draw(screen)
 
         pygame.display.flip()
 
-        # fps.tick(60)
         dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
