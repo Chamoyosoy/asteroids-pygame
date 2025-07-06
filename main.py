64195557c89e16a1bccbdb14b1b657927b8353a2
chamoyo <chamoyosoy@hotmail.com>
2025-07-05 21:40:10 -0600

    add player

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
 from constants import(
     SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS,
-    ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+    ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS,
+    PLAYER_RADIUS
 )
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # fps.tick(60)
+        dt = fps.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
